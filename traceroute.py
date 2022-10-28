5d8af1599015cedb682743b22a1dd160e2f3e098
thlau21 <thomaslau315@gmail.com>
2022-10-28 03:43:43 -0700

    I believe the project is completed

--- traceroute.py
+++ traceroute.py
@@ -1,17 +1,14 @@
 from scapy.all import *
 import ipaddress
-import random
 import sys
-import socket
-from scapy.layers.inet import IP, UDP, Ether
-from scapy.sendrecv import sr, sr1, sendp, send, srp
-from scapy.layers.l2 import Ether, ARP
+from scapy.layers.inet import IP, UDP
+from scapy.sendrecv import sr1
+from random import randint
+
 
 # random destination port for each probe
 def dportRand():
-    return random.randint(33343,33464)  
-sport = random.randint(1020, 65535)     # random high number for source port
-ipInRoute = []                          # list for ip addresses found
+    return randint(33343,33464)  
 
 def validate_ip(ip:str):
     #try to validate ip
@@ -21,14 +18,6 @@ def validate_ip(ip:str):
     except ValueError:
         print(f"IP '{ip}' is invalid!")
         sys.exit()
-
-# gets IP address from user   
-
-
-
-# so I think i need to make a packet and send it
-# after each time I send it, I'll record the response in the list (looking for IP addresses)
-# after each time I send it, I'll increment the hops, i.e., time to live
 
 if __name__ == "__main__":
     destinationIP = sys.argv[1]
@@ -41,11 +30,26 @@ if __name__ == "__main__":
     
     for TTL in range(1, maxHops+1):
         if flag is False:
+            #hide verbose of scapy
             conf.verb = 0
+            #create packet to send
             packetToSend = IP(ttl = TTL, dst = destinationIP)/UDP(dport = dportRand())
+            #send packet
             answer = sr1(packetToSend, timeout = 1)
             
+            #no reply from packet request
             if answer is None:
                 print('No answer!')
                 break
-                
+            #hit port unreachable
+            elif (answer.type == 3):
+                #target reached and TTL isn't done
+                if (targetReached == True and (TTL != maxHops + 1)):
+                    print(TTL, ".*")
+                elif(TTL != maxHops+1):
+                    print(answer.src)
+                    targetReached = True
+                else:
+                    break
+            else:
+                print(TTL, ". ", answer.src)
